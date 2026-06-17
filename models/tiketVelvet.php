eff153dd8c3f2926d99537462138dab6242a7739
ade-projects <projectalpha774@gmail.com>
2026-06-17 18:26:24 +0700

    [Tahap 5] tambah polimorfisme hitung total harga tiket velvet

--- models/tiketVelvet.php
+++ models/tiketVelvet.php
@@ -11,13 +11,31 @@ class TiketVelvet extends Tiket {
         string $jadwal_tayang, 
         int $jumlah_kursi, 
         int $hargaDasarTiket,
-        ?bool $bantalSelimutPack, 
-        ?bool $layananButler      
+        ?bool $bantalSelimutPack,
+        ?bool $layananButler
     ) {
         parent::__construct($id_tiket, $nama_film, $jadwal_tayang, $jumlah_kursi, $hargaDasarTiket);
         $this->bantalSelimutPack = $bantalSelimutPack;
         $this->layananButler = $layananButler;
     }
-    
+
+    public function getBantalSelimutPack(): ?bool {
+        return $this->bantalSelimutPack;
+    }
+
+    public function getLayananButler(): ?bool {
+        return $this->layananButler;
+    }
+
+    public function hitungTotalHarga(): int {
+        return (int) (($this->jumlah_kursi * $this->hargaDasarTiket) * 1.50);
+    }
+
+    public function tampilkanInfoFasilitas(): string {
+        $statusBantal = $this->bantalSelimutPack ? "Disediakan" : "Tidak Disediakan";
+        $statusButler = $this->layananButler ? "Tersedia" : "Tidak Tersedia";
+        
+        return "Paket Bantal & Selimut: " . $statusBantal . ", Layanan Butler: " . $statusButler;
+    }
 }
 ?>
